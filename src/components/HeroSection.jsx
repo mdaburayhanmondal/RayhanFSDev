6264f692b5ee8774ba59c8a890e83cfa79fa599c
Md. Abu Rayhan Mondal <mdaburayhanmondal@gmail.com>
2026-01-06 14:52:58 +0600

    -

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -45,7 +45,7 @@ const HeroSection = () => {
 
         <a
           href="public/Md_Abu_Rayhan_Mondal_CV.pdf"
-          download="Md_Abu_Rayhan_Mondal_CV.pdf"
+          download="public/Md_Abu_Rayhan_Mondal_CV.pdf"
           className="bg-transparent border border-white/20 text-white font-semibold px-8 py-3 rounded-full hover:bg-white/5 transition-all text-center"
         >
           Download CV
