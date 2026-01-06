7d71845ce75243d894e475f514ad195dc7b6c636
Md. Abu Rayhan Mondal <mdaburayhanmondal@gmail.com>
2026-01-07 00:45:02 +0600

    moved ThemeProvider in main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,18 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import ScrollToTopButton from './components/ScrollToTopButton';
-import { ThemeProvider } from './contexts/ThemeContext';
+// Remove ThemeProvider import
 
 function App() {
   return (
-    <ThemeProvider>
-      {/* bg-gray-50 = Light Mode Background
-         dark:bg-[#161513] = Dark Mode Background
-         text-gray-900 = Light Mode Text
-         dark:text-gray-900 dark:text-white = Dark Mode Text
-      */}
-      <div className="mx-auto overflow-hidden bg-gray-50 text-gray-900 dark:bg-[#161513] dark:text-gray-900 dark:text-white transition-colors duration-300 min-h-screen">
-        <Navbar />
-        <main className="min-h-screen pt-20">
-          <Outlet />
-        </main>
-        <ScrollToTopButton />
-        <Footer />
-      </div>
-    </ThemeProvider>
+    <div className="mx-auto overflow-hidden bg-gray-50 text-gray-900 dark:bg-[#161513] dark:text-white transition-colors duration-300 ease-in min-h-screen">
+      <Navbar />
+      <main className="min-h-screen pt-20">
+        <Outlet />
+      </main>
+      <ScrollToTopButton />
+      <Footer />
+    </div>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -8,18 +8,21 @@ import Contact from './pages/Contact.jsx';
 import Privacy from './pages/Privacy.jsx';
 import Terms from './pages/Terms.jsx';
 import Error from './pages/Error.jsx';
+import { ThemeProvider } from './contexts/ThemeContext';
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <Routes>
-      <Route element={<App />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/privacy" element={<Privacy />} />
-        <Route path="/terms" element={<Terms />} />
-      </Route>
-      <Route path="*" element={<Error />} />
-    </Routes>
-  </BrowserRouter>
+  <ThemeProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<App />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/privacy" element={<Privacy />} />
+          <Route path="/terms" element={<Terms />} />
+        </Route>
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
+  </ThemeProvider>
 );
